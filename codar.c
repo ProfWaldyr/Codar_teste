c25874f899fe2e8204e4ddf3f898ab73b9ed67c3
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-09 08:58:26 -0300

    .

--- codar.c
+++ codar.c
@@ -1,28 +1,30 @@
-#includde <stdio.h>
+#include <stdio.h>
 
 
 int main(){
     int idade;
     float altura;
-    char nome [50];
+    char nome[50];
     int matricula;
 
 
-    prinf("digite sua idade: \n");
+    printf("digite sua idade: \n");
     scanf("%d", &idade);
 
     printf("digite sua altura: \n");
-    scanf("%f", %altura);
+    scanf("%.2f", &altura);
 
     printf("digite seu nome: \n");
-    scanf("%s", %nome);
+    scanf("%s", &nome);
 
     printf("digite sua matricula: \n");
     scanf"%d", &matricula);
 
-    
+    printf("Matricula: %d \n", matricula);
+    printf("Nome do aluno: %s \n", nome);
+    printf("Idade do aluno: %d \n", idade);
+    prinf("Altura do aluno: %f \n", altura);
 
-
-
+    return 0;
 
 }
