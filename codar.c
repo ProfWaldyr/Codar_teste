795ea0f9a68ca5103166026835f04be66928b958
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-09-09 07:54:27 -0300

    .

--- codar.c
+++ codar.c
@@ -1,1 +1,21 @@
+#includde <stdio.h>
 
+
+int main(){
+    int idade;
+    float altura;
+    char nome [50];
+    int matricula;
+
+
+    prinf("digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("digite sua altura: \n");
+    scanf("%f", %float);
+
+    
+
+
+
+}
